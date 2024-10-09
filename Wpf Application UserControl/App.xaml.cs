1c23da6857692e82fd66ae6c3e7cfa71fc5073bc
UesliLaskajs <uesli.laska@gmail.com>
2024-10-09 18:23:24 +0200

    UserControl

---
+++ Wpf Application UserControl/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Wpf_Application_UserControl
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
